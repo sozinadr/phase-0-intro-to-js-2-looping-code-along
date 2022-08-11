f0bcc2631eabcd65761a870152eebf8e95b9d435
sozi <sozyarnadir@gmail.com>
2022-08-12 01:49:41 +0300

    Assignment Completed

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+
+function writeCards(names, event) {
+    const cards = [];
+
+    for (let i = 0; i < names.length; i++) {
+        cards.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return cards;
+}
+
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number--);
+    }
+}
